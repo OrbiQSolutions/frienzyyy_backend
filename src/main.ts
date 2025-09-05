90873215d298060f2677bef9fa173c211e520379
Drako-Rexon <abhishek491981@gmail.com>
2025-09-05 22:27:03 +0530

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Sequelize } from 'sequelize-typescript';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Frienzyyy backend')
+    .setDescription('The frienzyyy app which is a dating app')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const sequelize = app.get(Sequelize);
   await sequelize.sync({ alter: true });
