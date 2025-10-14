c07f523e96765e1eb476ae7961656a3983e6651f
Drako-Rexon <abhishek491981@gmail.com>
2025-10-14 22:15:14 +0530

    CHAT - websocket chat is saving in DB

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ChatMessages } from './entities/chat.entity';
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([ChatMessages])
+  ],
   providers: [ChatGateway, ChatService],
 })
-export class ChatModule {}
+export class ChatModule { }
